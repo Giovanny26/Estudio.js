63dd0733d153f427828584c93b42c1896eade219
Giovanny26 <brayan26cd@gmail.com>
2021-03-24 12:46:22 -0500

    Mi primer repositorio

---
+++ programacionImperativa/Operadores.js
@@ -0,0 +1,37 @@
+
+console.log ( 5 + 4 );//suma
+
+console.log ( 5 - 4 );//resta
+
+console.log ( 3 * 4 );//multiplicacion
+
+console.log ( 8 / 2 );//division
+
+console.log ( 9 % 2 );//residuo
+
+var numero = 6;
+console.log (++numero);//suma 1 a variable
+
+var numero1 = 8;
+console.log (--numero1);//resta uno a variable
+
+let ocho = 8;
+let nueve = 9;
+let siete = 7;
+let seis = "6"
+let cinco = "5"
+
+console.log (ocho == ocho);//igual que valor
+console.log (nueve === cinco);// igual que dato y valor
+console.log (ocho != ocho);// no igual que valor
+console.log (seis !== siete);// no igual que dato y valor
+console.log (ocho > siete);// mayor
+console.log (nueve >= nueve);// mayor o igual
+console.log (nueve < ocho);// menor 
+console.log (ocho <= siete);// menor o igual
+
+console.log (ocho == ocho && siete < ocho);// dos de estas se cumplen
+console.log (siete >= ocho || ocho !== ocho);// solo una de ellas se cumplen
+console.log (seis + " " + cinco);
+
+
